eb580b8ed61a1373061643517a38c9ceb7b4ee6f
Brahim <brahimcasas@hotmail.es>
2021-08-12 17:42:37 +0200

    Feat: db seed employees and products closes FeatB: create db seed functions mhfortuna/node-develop-your-mvc-project#3

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 const app = require("./server");
 const { config } = require("./config/config");
 const connect = require("./db/connect");
+const { seedProducts, seedEmployees } = require("./db/seed");
 
 connect()
   .then(async () => {
+    // await seedProducts();
+    // await seedEmployees();
+
     app.listen(config.app.port, () => {
       console.log(`Server running at port ${config.app.port}`);
     });
