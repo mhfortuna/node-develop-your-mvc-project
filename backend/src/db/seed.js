acc248941c2d3ba511a4e1ea578c07d28e9fc224
Brahim <brahimcasas@hotmail.es>
2021-08-12 15:57:06 +0200

    Add products json

---
+++ backend/src/db/seed.js
@@ -0,0 +1,16 @@
+const db = require("../models");
+
+const { getSeedProducts } = require("./data-products");
+
+async function seedProducts() {
+  //TODO:
+
+  await db.Product.deleteMany({});
+  await db.Product.create([...results]);
+}
+
+async function seedEmployees() {
+  //TODO:
+  await db.Employee.deleteMany({});
+  await db.Employee.create([...results]);
+}
