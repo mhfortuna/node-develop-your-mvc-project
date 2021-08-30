31c82a57fe48a01a8f0819713773ed31e87fe6e8
Nachomontoya <montoyanacho@gmail.com>
2021-08-30 14:23:05 +0200

    main routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { PUBLIC, PRIVATE } from "./constants/routes";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-headser">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-headser">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path={PRIVATE.DASHBOARD_PRODUCTS} />
+        <Route path={PRIVATE.DASHBOARD_USERS} />
+        <Route path={PUBLIC.SUMMARY} />
+        <Route path={PUBLIC.PAYMENT} />
+        <Route path={PUBLIC.SHIPPING} />
+        <Route path={PUBLIC.SUMMARY} />
+        <Route path={PUBLIC.USER_EDIT} />
+        <Route path={PUBLIC.USER_INFO} />
+        <Route path={PUBLIC.SIGNIN} />
+        <Route path={PUBLIC.SIGNUP} />
+        <Route path={PUBLIC.PRODUCT} />
+        <Route path={PRIVATE.ADMIN_SIGNIN} />
+        <Route path={PUBLIC.HOME} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/constants/routes.js
@@ -0,0 +1,22 @@
+//! PUBLIC PAGES
+
+export const PUBLIC = {
+  HOME: "/",
+  PRODUCT: "/product",
+  SHOPPING_CART: "/shopping-cart",
+  SHIPPING: "/checkout/shipping",
+  PAYMENT: "/checkout/payment",
+  SUMMARY: "/checkout/summary",
+  SIGNIN: "/signin",
+  SIGNUP: "/signup",
+  USER_INFO: "/user",
+  USER_EDIT: "/user/edit",
+};
+
+export const PRIVATE = {
+  ADMIN_SIGNIN: "/admin",
+  DASHBOARD_USERS: "/admin/dashboard/users",
+  DASHBOARD_PRODUCTS: "/admin/dashboard/products",
+};
+
+//! PRIVATE PAGES
