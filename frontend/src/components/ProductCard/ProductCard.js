9f2fd4f4ba93e7b654a9be63ee8366a691e043b5
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-08-31 17:15:23 +0200

    Finished ProductCard and ProductList

--- frontend/src/components/ProductCard/ProductCard.js
+++ frontend/src/components/ProductCard/ProductCard.js
@@ -1,30 +1,33 @@
 import React from "react";
+import "./ProductCard.scss";
 
 export default function ProductCard({ product }) {
   return (
     <div className="col">
       <div className="card h-100 border-0">
-        <img
-          className="card-img-top p-4 border"
-          src={product.images.main}
-          alt={`${product.title}`}
-        />
-        <div className="card-body">
-          <h5 className="text-center fw-bold">{product.title}</h5>
+        <div className="border product-card-img-container d-flex align-items-center justify-content-center">
+          <img
+            className="px-4 product-card-img img-fluid"
+            src={product.images.main}
+            alt={`${product.title}`}
+          />
+        </div>
+        <div className="card-body p-0 py-3">
+          <h5 className="font-bold medium-big-text text-truncate">
+            {product.title}
+          </h5>
           <div className="row">
-            <div className="col-6">
-              {product.unitsInStock > 0 && <p className="fw-bold">In stock</p>}
+            <div className="col-6 pe-0">
+              {product.unitsInStock > 0 && (
+                <p className="intermediate-text">In stock</p>
+              )}
             </div>
-            <div className="col-6">
-              <p className="fw-bold fs-5 text-end">$ {product.price}</p>
+            <div className="col-6 ps-0">
+              <p className="text-end intermediate-text font-bold">
+                ${product.price.toFixed(2)}
+              </p>
             </div>
           </div>
-          <p className="fs-6">
-            Available lenses:{" "}
-            {product.lens.map((item, index) =>
-              index + 1 === product.lens.length ? `${item}mm ` : `${item}mm, `,
-            )}
-          </p>
         </div>
       </div>
     </div>
