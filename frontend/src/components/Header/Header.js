1d129f758341cca4f54577c192ff2e42a65929ca
Ricard Garcia <imricardgarcia@gmail.com>
2021-08-31 13:13:42 +0200

    added Header component props

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -5,7 +5,7 @@ import "./Header.scss";
 export default function Header({ pageTitle, isLogged }) {
   return (
     <header className="container-fluid m-0 row">
-      <div className="col col-8 title p-0 font-bold ">{pageTitle}</div>
+      <div className="col col-8 title p-0 font-bold">{pageTitle}</div>
       {isLogged ? (
         <div className="col col-4 d-flex p-0 user-wrapper justify-content-end align-items-start">
           <div className="user-name font-bold medium-text">Username</div>
