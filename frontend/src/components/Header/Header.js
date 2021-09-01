bee7ea5bb6fa7478c5f0c0afa9bdd2a9b7023048
Nacho <73990495+Nachomontoya@users.noreply.github.com>
2021-09-01 17:00:24 +0200

    cartIcon uploaded on Header (#46)

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,14 +1,30 @@
 import React from "react";
+import Button from "../Button/Button";
+import CartIcon from "../SVGIcons/CartIcon";
 
 import "./Header.scss";
 
-export default function Header({ pageTitle, isLogged }) {
+export default function Header({ pageTitle, isLogged, cartItems }) {
   return (
     <header className="row m-0">
       <div className="col col-8 title p-0 font-bold">{pageTitle}</div>
       {isLogged ? (
         <div className="col col-4 d-flex p-0 user-wrapper justify-content-end align-items-start">
           <div className="user-name font-bold medium-text">Username</div>
+          {cartItems ? (
+            <div className="ms-3">
+              <Button transparent>
+                <CartIcon />
+              </Button>
+            </div>
+          ) : (
+            <div className="ms-3">
+              <Button transparent disabled>
+                <CartIcon />
+              </Button>
+            </div>
+          )}
+
           <div className="ms-3 btn btn-outline-dark medium-text">Log out</div>
         </div>
       ) : (
