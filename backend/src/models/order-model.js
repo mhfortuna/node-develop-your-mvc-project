c15cad4af5cc70c45b55724390794126ccab7719
Brahim <brahimcasas@hotmail.es>
2021-09-02 16:42:46 +0200

    Add order model

--- backend/src/models/order-model.js
+++ backend/src/models/order-model.js
@@ -3,22 +3,34 @@ const { Schema } = require("mongoose");
 
 const orderSchema = new Schema(
   {
-    client_id: {
-      type: mongoose.SchemaTypes.ObjectId,
-      ref: "client",
-      required: [true, "client id is required"],
+    // client_id: {
+    //   type: mongoose.SchemaTypes.ObjectId,
+    //   ref: "client",
+    //   required: [true, "client id is required"],
+    // },
+    // products: {
+    //   type: [
+    //     {
+    //       type: mongoose.SchemaTypes.ObjectId,
+    //       ref: "product",
+    //       required: [true, "At least one product is required"],
+    //     },
+    //   ],
+    //   required: [true, "Products is required"],
+    // },
+    // price: { type: Number },
+    firstName: {
+      type: String,
+      required: [true, "First name is required"],
     },
-    products: {
-      type: [
-        {
-          type: mongoose.SchemaTypes.ObjectId,
-          ref: "product",
-          required: [true, "At least one product is required"],
-        },
-      ],
-      required: [true, "Products is required"],
-    },
-    price: { type: Number },
+    lastName: { type: String },
+    phoneNumber: { type: String },
+    email: { type: String, required: [true, "Email is required"] },
+    address: { type: String },
+    zipCode: { type: String },
+    city: { type: String },
+    country: { type: String },
+    products: { type: Map },
   },
 
   {
