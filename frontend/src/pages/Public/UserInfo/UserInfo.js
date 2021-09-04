d165c4dcd2a5381ec644de63287920d4a073a856
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-09-04 09:35:02 +0200

    Load user data in form

--- frontend/src/pages/Public/UserInfo/UserInfo.js
+++ frontend/src/pages/Public/UserInfo/UserInfo.js
@@ -1,34 +1,90 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory, useRouteMatch } from "react-router-dom";
+import { useFormik } from "formik";
+
 import FloatInput from "../../../components/FloatInput";
 import Button from "../../../components/Button";
 
+import userSchema from "./user-schema";
+
+import { PUBLIC } from "../../../constants/routes";
+
 import withLayout from "../../../hoc/withLayout";
 
+import { getClient } from "../../../api";
+
 function UserInfo() {
-  const [name, setName] = useState();
   const [phone, setPhone] = useState();
+  const [loadStatus, setLoadStatus] = useState({
+    isError: false,
+    isLoading: true,
+  });
+  const { clientId } = useRouteMatch(`${PUBLIC.USER_INFO}/:clientId`).params;
+
+  const history = useHistory();
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+      email: "",
+      address: "",
+      zipCode: "",
+      city: "",
+      country: "",
+    },
+    validationSchema: userSchema,
+    enableReinitialize: true,
+    onSubmit: (accountState) => {
+      // updateCheckoutContext(shippingState);
+      // history.push(PUBLIC.PAYMENT);
+      console.log(accountState);
+      console.log(history);
+    },
+  });
+
+  async function loadClientData(id) {
+    try {
+      const { data } = await getClient(id);
+      formik.setValues({
+        address: data.data.address || "",
+        city: data.data.city || "",
+        country: data.data.country || "",
+        email: data.data.email || "",
+        firstName: data.data.firstName || "",
+        lastName: data.data.lastName || "",
+        phoneNumber: data.data.phoneNumber || "",
+        zipCode: data.data.zipCode || "",
+      });
+      setLoadStatus({ isError: false, isLoading: false });
+    } catch (error) {
+      setLoadStatus({ isError: true, isLoading: false, error: error });
+    }
+  }
+
+  useEffect(() => {
+    // if (phoneNumber) setPhone(phoneNumber);
+    loadClientData(clientId);
+  }, []);
 
   const handleChangePhone = (e) => {
     const { value, maxLength } = e.target;
-    const phoneNumber = value.slice(0, maxLength);
+    const pNumber = value.slice(0, maxLength);
 
     if (e.nativeEvent.inputType === "deleteContentBackward") {
-      setPhone(phoneNumber);
+      setPhone(pNumber);
+      formik.values.phoneNumber = pNumber.replace(/\s+/g, "");
       return;
     }
 
-    const x = phoneNumber
-      .replace(/\D/g, "")
-      .match(/(\d{0,3})(\d{0,3})(\d{0,3})/);
+    const x = pNumber.replace(/\D/g, "").match(/(\d{0,3})(\d{0,3})(\d{0,3})/);
     const pattern = `${x[1] ?? x[1]} ${x[2] ?? x[2]} ${x[3] ?? x[3]}`;
 
     if (pattern.length > 2) {
       setPhone(pattern);
     }
-  };
-
-  const handleChangeName = (e) => {
-    setName(e.target.value);
+    formik.values.phoneNumber = pNumber.replace(/\s+/g, "");
   };
 
   return (
@@ -38,14 +94,23 @@ function UserInfo() {
         <form>
           <div className="row">
             <div className="col col-6">
+              {!loadStatus.isLoading && !loadStatus.isError && (
+                <h3>Data loaded</h3>
+              )}
+              {loadStatus.isLoading && !loadStatus.isError && (
+                <h3>Currently loading...</h3>
+              )}
+              {loadStatus.isError && !loadStatus.isLoading && <h3>ERROR</h3>}
               <FloatInput
                 id="firstName"
                 type="text"
                 label="First Name"
                 placeholder="First name"
-                value={name}
-                handleChange={handleChangeName}
-                handleBlur={handleChangeName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.firstName}
+                errorMessage={formik.errors.firstName}
+                hasErrorMessage={formik.touched.firstName}
               />
             </div>
             <div className="col col-6">
@@ -54,6 +119,11 @@ function UserInfo() {
                 type="text"
                 label="Last name"
                 placeholder="Last Name"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.lastName}
+                errorMessage={formik.errors.lastName}
+                hasErrorMessage={formik.touched.lastName}
               />
             </div>
 
@@ -65,7 +135,10 @@ function UserInfo() {
                 placeholder="Phone Number"
                 maxLength={11}
                 value={phone}
-                handleChange={handleChangePhone}
+                onChange={handleChangePhone}
+                onBlur={formik.handleBlur}
+                errorMessage={formik.errors.phoneNumber}
+                hasErrorMessage={formik.touched.phoneNumber}
               />
             </div>
             <div className="col col-6">
@@ -74,6 +147,11 @@ function UserInfo() {
                 type="email"
                 label="Email address"
                 placeholder="example@example.com"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+                errorMessage={formik.errors.email}
+                hasErrorMessage={formik.touched.email}
               />
             </div>
 
@@ -83,6 +161,11 @@ function UserInfo() {
                 type="address"
                 label="Address"
                 placeholder="address"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.address}
+                errorMessage={formik.errors.address}
+                hasErrorMessage={formik.touched.address}
               />
             </div>
 
@@ -93,6 +176,11 @@ function UserInfo() {
                 label="Zip code"
                 placeholder="zipCode"
                 maxLength={8}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.zipCode}
+                errorMessage={formik.errors.zipCode}
+                hasErrorMessage={formik.touched.zipCode}
               />
             </div>
             <div className="col col-4">
@@ -101,6 +189,11 @@ function UserInfo() {
                 type="text"
                 label="City"
                 placeholder="city"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.city}
+                errorMessage={formik.errors.city}
+                hasErrorMessage={formik.touched.city}
               />
             </div>
             <div className="col col-4">
@@ -109,13 +202,20 @@ function UserInfo() {
                 type="text"
                 label="Country"
                 placeholder="country"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.country}
+                errorMessage={formik.errors.country}
+                hasErrorMessage={formik.touched.country}
               />
             </div>
           </div>
 
           <div className="d-flex mt-5">
             <div className="d-inline-block ms-auto">
-              <Button black>Edit</Button>
+              <Button submitButton black>
+                Edit
+              </Button>
             </div>
           </div>
         </form>
